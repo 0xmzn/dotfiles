b9d663851d2dab0ff59ba904d0095ef5e2d28c97
Mazen Saad <mmes3666@gmail.com>
2021-10-25 14:50:04 +0200

    Fixed dmenuPlay with exit status

--- scripts/dmenuPlay.sh
+++ scripts/dmenuPlay.sh
@@ -5,6 +5,7 @@ vidDir=$(xdg-user-dir VIDEOS)
 while [[ -d $vidDir ]]
 do
     chosenDir=$( ls $vidDir | dmenu -l 4 )
+    [[ $? -eq 1 ]] && exit 1
     vidDir="$vidDir/$chosenDir"
     subDirCount=$(find $vidDir -maxdepth 1 -type d | wc -l)
     [[ $subDirCount -eq 1 ]] && break
