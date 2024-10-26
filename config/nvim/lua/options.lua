12e5bae14ecf14a43805a06c4fbfcfc2e841cd9d
Mazen Saad <mmes3666@gmail.com>
2024-10-26 15:04:43 +0200

    add neovim expandtab true

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -5,6 +5,7 @@ vim.o.splitright = true
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
+vim.o.expandtab = true
 vim.o.smarttab = true
 vim.o.wrap = false
 vim.o.clipboard = "unnamedplus"
