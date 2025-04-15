295e1cc4f69b1d417b0cb6676ef19539b317c9f7
Mazen Saad <mmes3666@gmail.com>
2025-04-15 08:27:46 +0200

    add telescope.nvim

--- config/nvim/lua/0xmzn/init.lua
+++ config/nvim/lua/0xmzn/init.lua
@@ -17,5 +17,10 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+require("lazy").setup({
+    spec = "0xmzn.plugins",
+    change_detection = { notify = false }
+})
 
-require("lazy").setup("0xmzn.plugins")
+-- has to be after lazy plugins are loaded
+require("0xmzn.keybinds")
